5a22772294aeb3412876398293d0a40205f9c382
miqo1385 <miqo1385@gmail.com>
2024-03-25 20:28:03 -0700

    done

---
+++ components/src/Card.js
@@ -0,0 +1,14 @@
+function Card(){
+
+    return(
+        <div className="card">
+            <img className="card-image" src="https://via.placeholder.com/150" alt="dog picture"></img>
+            <h1 className="card-title">Salsichinha Quintana</h1>
+            <p className="card-text">Un perrito muy lindo y chiquitito</p>
+
+
+        </div>
+    );
+}
+
+export default Card;
