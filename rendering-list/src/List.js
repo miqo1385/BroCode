ec7a1c53bed80d21b992a95ee9dc60042a738625
miqo1385 <miqo1385@gmail.com>
2024-03-27 22:38:07 -0700

    rendering list

---
+++ rendering-list/src/List.js
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+
+function List(props){
+    const category = props.category;
+    const itemList = props.items;
+
+
+
+    // This will sort the list by alphabetic order
+    //fruits.sort((a,b) => a.name.localeCompare(b.name));
+    // fruits.sort((a,b) => b.name.localeCompare(a.name)); //reverse alphabetic
+    // fruit.sort((a,b) => a.calories - b.calories); // numerical
+    // fruit.sort((a,b) => b.calories - a.calories); // reverse numerical
+
+    // this will filter fruits with calories under 100
+    //const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+
+
+    const listItems = itemList.map(item => <li key={item.id}>
+                                                      {item.name}: &nbsp;
+                                                       <b>{item.calories}</b></li>);
+    return(<>
+        <h3 className='list-category'>{category}</h3>
+        <ol className='list-items'>{listItems}</ol>
+    </>);
+
+}
+List.prototype = {
+    category: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.number, name: PropTypes.string, calories: PropTypes.number }))
+}
+List.defaultProps = {
+    category: "Category",
+    items: [],
+}
+
+export default List
