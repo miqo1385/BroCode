5a1d53a1fdc212494fe33b6ebc033b660e4170df
miqo1385 <miqo1385@gmail.com>
2024-03-28 20:00:16 -0700

    clicking event

---
+++ clicking-event/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from "./Button";
+import ProfilePicture from "./ProfilePicture";
+
+function App() {
+  return (
+    <ProfilePicture></ProfilePicture>
+  );
+}
+
+export default App;
---
+++ clicking-event/src/Button.js
@@ -0,0 +1,14 @@
+
+
+function Button(){
+
+    const handleClick = (e) => e.target.textContent = 'OUCH!';
+
+
+
+    return(<button onDoubleClick ={(e) => handleClick(e)}>Click me</button>);
+
+
+}
+
+export default Button
