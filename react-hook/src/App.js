8c8226af5fb60b727c996ed4d55a3d44afeeed39
miqo1385 <miqo1385@gmail.com>
2024-03-28 22:49:34 -0700

    useState React hook

---
+++ react-hook/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from "./MyComponent";
+import Counter from "./Counter";
+
+function App() {
+  return (
+   <Counter></Counter>
+  );
+}
+
+export default App;
